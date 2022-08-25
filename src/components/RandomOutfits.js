1de7370ae3afd56dff6b632d090a2118773ea307
Jaden Lund <lundjaden@gmail.com>
2022-08-25 13:16:52 -0400

    some styling to buttons

--- src/components/RandomOutfits.js
+++ src/components/RandomOutfits.js
@@ -6,8 +6,9 @@ function RandomClothing({ outfits }) {
     let [favorite, setFavorite] = useState(false)
 
     function addFavorites() {
+        console.log(outfits)
         //on the click of add to favorites, move to the favorites class
-        console.log('hi')
+        console.log('hi', outfits[0], favorite)//outfit at the area
         setFavorite(!favorite)
         //pass to the child
     }
@@ -15,7 +16,7 @@ function RandomClothing({ outfits }) {
         <div>
             <h1>Get Inspired by Others Outfits</h1>
             <h2 className='favorites'>Favorites</h2>
-            <div>{favorite ? <RenderRandom /> : null}</div>
+            {/* <div>{favorite ? <RenderRandom  /> : null}</div> */}
             <div className='random-outfits'>
                 {outfits.map(outfit => <RenderRandom outfit={outfit} content={outfit.content} key={outfit.id} addFavorites={addFavorites} />)}
             </div>
