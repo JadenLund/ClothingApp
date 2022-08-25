a2613f129fc0898d8aaa19c8f4122ac559578d15
Jaden Lund <lundjaden@gmail.com>
2022-08-25 12:29:31 -0400

    more work on favorites button, changed icon and title

--- src/components/RandomOutfits.js
+++ src/components/RandomOutfits.js
@@ -1,19 +1,23 @@
 import { buildTimeValue } from '@testing-library/user-event/dist/utils'
-import React from 'react'
+import React, { useState } from 'react'
 import RenderRandom from './RenderRandom'
 
 function RandomClothing({ outfits }) {
+    let [favorite, setFavorite] = useState(false)
 
     function addFavorites() {
         //on the click of add to favorites, move to the favorites class
-        
+        console.log('hi')
+        setFavorite(!favorite)
+        //pass to the child
     }
     return (
         <div>
             <h1>Get Inspired by Others Outfits</h1>
             <h2 className='favorites'>Favorites</h2>
+            <div>{favorite ? <RenderRandom /> : null}</div>
             <div className='random-outfits'>
-                {outfits.map(outfit => <RenderRandom outfit={outfit} content={outfit.content} key={outfit.id} />)}
+                {outfits.map(outfit => <RenderRandom outfit={outfit} content={outfit.content} key={outfit.id} addFavorites={addFavorites} />)}
             </div>
         </div>
     )
