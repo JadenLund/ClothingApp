6b586e208002e6eaba78c38e6a36560de2434c8e
John Carroll <johndavidcarroll@gmail.com>
2022-08-25 17:17:30 -0400

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -104,11 +104,11 @@ function App() {
 
             <Route path='/project1'>
             <div className='sidebar'>
-            <button onClick={handleShirtFormClick}>Add New Shirt</button>
+            <button className='add-new-shirt' onClick={handleShirtFormClick}>Add New Shirt</button>
             {showShirtForm ? null : <NewShirtForm handleShirtSubmit={handleShirtSubmit}/>}
         </div>
         <div className='sidebar-2'>
-        <button onClick={handlePantsFormClick}> Add New Pants</button>
+        <button className='add-new-pants' onClick={handlePantsFormClick}> Add New Pants</button>
             {showPantsForm ? null : <NewPantsForm handlePantsSubmit={handlePantsSubmit}/>}
         </div>
                 <ClothingContainer outfits={outfits} pants={pants} shirts={shirts} />
