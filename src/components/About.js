7d5844a092d87a78e63876b14c992da154b6eba8
Jaden Lund <lundjaden@gmail.com>
2022-08-26 00:23:10 -0400

    Styled the About section

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,11 @@ function About() {
         <div>
             <h1>Welcome to the Clothing App!</h1>
             <h2>Built by Jaden L and John C</h2>
-            <p>This application was built to help you decide your outfit for the day, </p>
-            <p>and inspire you to try a new style</p>
+            <h3>This application was built to help you decide your outfit for the day, </h3>
+            <h3>and inspire you to try a new style</h3>
             <div className='images'>
-                <img src='https://econsultancy.imgix.net/content/uploads/2018/08/10180249/9_ASOS_flyer.jpg' />
+                <img src='https://static.vecteezy.com/system/resources/previews/005/084/913/non_2x/set-of-sketches-of-beautiful-and-diverse-male-fashion-outfits-free-vector.jpg' />
+                <img src='https://img.freepik.com/free-vector/set-sketches-beautiful-diverse-female-fashion-outfits_1150-52764.jpg' />
             </div>
         </div>
     )
